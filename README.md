0c921f41fef95efabb4e25bc8316c5f23be8fb03
John Keller <JTKeller@gmail.com>
2016-02-26 13:35:41 -0600

    Added files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+README
+==========================================
+
+This is the Getting and Cleaning Data Project
+
+The R script, `run_analysis.R`, does the following:
+
+1) Download and unzip
+
+2) Merge the data
+
+3) Extract the mean and Standard Deviation for each measurement
+
+4) name the activities
+
+5) label the data set
+
+6) Create tidy data set
+
+## Files
+
+`CodeBook.md` describes the variables, the data, and any transformations.
+
+`run_analysis.R` contains all the code to perform the analyses.
+
+The output is called `tidydata.txt`.
---
+++ run_analysis.R
@@ -0,0 +1,85 @@
+library(plyr)
+library(reshape2)
+
+
+## Download and unzip
+
+filename <- "getdata_dataset.zip"
+
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename)
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+
+
+# Merge the data
+
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+# create 'x' data set
+x_data <- rbind(x_train, x_test)
+
+# create 'y' data set
+y_data <- rbind(y_train, y_test)
+
+# create 'subject' data set
+subject_data <- rbind(subject_train, subject_test)
+
+
+
+# Extract the mean and Standard Deviation for each measurement
+
+features <- read.table("UCI HAR Dataset/features.txt")
+
+# get only columns with mean() or std() in their names
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+
+# subset the desired columns
+x_data <- x_data[, mean_and_std_features]
+
+# correct the column names
+names(x_data) <- features[mean_and_std_features, 2]
+
+
+
+
+# name the activities
+
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+# update values with correct activity names
+y_data[, 1] <- activities[y_data[, 1], 2]
+
+# correct column name
+names(y_data) <- "activity"
+
+
+
+
+# label the data set
+
+# correct column name
+names(subject_data) <- "subject"
+
+# bind all the data in a single data set
+all_data <- cbind(x_data, y_data, subject_data)
+
+
+
+
+# Create tidy data set 
+
+# 66 <- 68 columns but last two (activity & subject)
+tidydata <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+
+write.table(tidydata, "tidydata.txt", row.name=FALSE)
